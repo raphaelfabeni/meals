2fa4ea93861ea988b188ceb58da3069845ec3f36
Raphael Fabeni <rapha.fabeni@gmail.com>
2026-03-28 19:13:35 -0400

    feat: create workshop-starter branch with empty structure

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,22 +1,13 @@
 import "./globals.css";
-import NewRelicAgent from "@/app/_lib/NewRelicAgent";
 
 export const metadata = {
-  title: "Meal Helper",
+  title: "Meal Finder",
   description: "Find recipes fast, keep things easy.",
-  robots: {
-    index: false,
-    follow: false,
-  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <head>
-        <meta name="robots" content="noindex, nofollow" />
-        <NewRelicAgent />
-      </head>
       <body className="antialiased text-gray-900">
         {children}
       </body>
